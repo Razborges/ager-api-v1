c89e1bacf9d2f86541b70d05943bcac697b467b5
Razborges <razborges@me.com>
2018-05-06 02:40:40 -0300

    Validate params in userController

--- src/controllers/robot.js
+++ src/controllers/robot.js
@@ -5,12 +5,13 @@ const validator = require('validator');
 const Robot = db.sequelize.import('../models/robot');
 const router = express.Router();
 
+const { regex } = require('../config/regex');
+
 router.post('/', (req, res) => {
   const { numberSeries, name } = req.body;
 
-  const reg = new RegExp('[a-z]{5}[0-9]{3}$');
   const numberLength = validator.isLength(numberSeries, { min: 8, max: 8 });
-  const numberValid = reg.test(numberSeries);
+  const numberValid = regex.test(numberSeries);
   const nameValid = validator.isEmpty(name);
 
   if (numberValid && numberLength && !nameValid) {
@@ -50,15 +51,27 @@ router.get('/', (req, res) => {
 
 router.get('/:numberSeries', (req, res) => {
   const { numberSeries } = req.params;
-  Robot.findOne({ where: { numberSeries } })
-    .then(result => res.status(200).json({
-      error: false,
-      data: result,
-    }))
-    .catch(error => res.status(404).json({
+
+  const numberLength = validator.isLength(numberSeries, { min: 8, max: 8 });
+  const numberValid = regex.test(numberSeries);
+
+  if (numberValid && numberLength) {
+    Robot.findOne({ where: { numberSeries } })
+      .then(result => res.status(200).json({
+        error: false,
+        data: result,
+      }))
+      .catch(error => res.status(404).json({
+        error: true,
+        type: error,
+      }));
+  } else {
+    res.status(400).json({
       error: true,
-      type: error,
-    }));
+      data: [],
+      type: 'Número de série inválido.',
+    });
+  }
 });
 
 module.exports = router;
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const db = require('../config/db');
+const validator = require('validator');
 
 const User = db.sequelize.import('../models/user');
 const Robot = db.sequelize.import('../models/robot');
 const router = express.Router();
+
+const { regex } = require('../config/regex');
 
 router.post('/:numberSeries', async (req, res) => {
   const { numberSeries } = req.params;
@@ -11,31 +14,52 @@ router.post('/:numberSeries', async (req, res) => {
     name, email, service, serviceId,
   } = req.body;
 
-  const robot = await Robot.findOne({ where: { numberSeries } });
+  const numberLength = validator.isLength(numberSeries, { min: 8, max: 8 });
+  const numberValid = regex.test(numberSeries);
+  const nameValid = validator.isEmpty(name);
+  const emailValid = validator.isEmail(email);
+  const serviceValid = validator.isEmpty(service);
+  const serviceIdValid = validator.isEmpty(serviceId);
 
-  if (!robot) {
-    res.status(404).json({
-      error: true,
-      data: [],
-    });
-  }
+  if (numberValid && numberLength && !nameValid && emailValid && !serviceValid && !serviceIdValid) {
+    const robot = await Robot.findOne({ where: { numberSeries } });
 
-  if (robot) {
-    const user = User.build({
-      name, email, service, serviceId,
-    });
-    user.setRobot(robot);
-
-    user.save()
-      .then(result => res.status(201).json({
-        error: false,
-        data: result,
-      }))
-      .catch(error => res.status(501).json({
+    if (!robot) {
+      res.status(404).json({
         error: true,
         data: [],
-        type: error,
-      }));
+      });
+    }
+
+    if (robot) {
+      const user = User.build({
+        name, email, service, serviceId,
+      });
+      user.setRobot(robot);
+
+      user.save()
+        .then(result => res.status(201).json({
+          error: false,
+          data: result,
+        }))
+        .catch(error => res.status(501).json({
+          error: true,
+          data: [],
+          type: error,
+        }));
+    }
+  } else {
+    const error1 = nameValid ? 'Obrigatório preenchimento do nome.' : '';
+    const error2 = !numberValid || !numberLength ? 'Número de série inválido.' : '';
+    const error3 = !emailValid ? 'Utilize um e-mail válido.' : '';
+    const error4 = serviceValid ? 'Obrigatório o preenchimento do nome do serviço.' : '';
+    const error5 = serviceIdValid ? 'Obrigatório o preenchimento do id do serviço.' : '';
+
+    res.status(400).json({
+      error: true,
+      data: [],
+      type: `${error1}${error2}${error3}${error4}${error5}`,
+    });
   }
 });
 
