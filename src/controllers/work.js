9f0527697a5e9fa511466c9afb56c736e2ea0a07
Razborges <razborges@me.com>
2018-05-06 02:41:23 -0300

    Validate params in workController

--- src/controllers/work.js
+++ src/controllers/work.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../config/db');
 const moment = require('moment');
+const validator = require('validator');
 
 const Work = db.sequelize.import('../models/work');
 const Route = db.sequelize.import('../models/route');
@@ -15,34 +16,52 @@ router.post('/:idRoute', async (req, res) => {
   const initDate = moment(startWork, 'DD/MM/YYYY hh:mm:ss').utc();
   const endDate = moment(endWork, 'DD/MM/YYYY hh:mm:ss').utc();
 
-  const route = await Route.findById(idRoute);
+  const tempValid = validator.isNumeric(temperature);
+  const humidValid = validator.isNumeric(humidity);
+  const startValid = initDate.isValid();
+  const endValid = endDate.isValid();
 
-  if (!route) {
-    res.status(404).json({
-      error: true,
-      data: [],
-    });
-  }
+  if (tempValid && humidValid && startValid && endValid) {
+    const route = await Route.findById(idRoute);
 
-  if (route) {
-    const work = await Work.build({
-      temperature,
-      humidity,
-      startWork: initDate,
-      endWork: endDate,
-    });
-    work.setRoute(route);
-
-    work.save()
-      .then(result => res.status(201).json({
-        error: false,
-        data: result,
-      }))
-      .catch(error => res.status(501).json({
+    if (!route) {
+      res.status(404).json({
         error: true,
         data: [],
-        type: error,
-      }));
+      });
+    }
+
+    if (route) {
+      const work = await Work.build({
+        temperature,
+        humidity,
+        startWork: initDate,
+        endWork: endDate,
+      });
+      work.setRoute(route);
+
+      work.save()
+        .then(result => res.status(201).json({
+          error: false,
+          data: result,
+        }))
+        .catch(error => res.status(501).json({
+          error: true,
+          data: [],
+          type: error,
+        }));
+    }
+  } else {
+    const error1 = !tempValid ? 'Obrigatório preenchimento da temperatura com um valor numérico.' : '';
+    const error2 = !humidValid ? 'Obrigatório preenchimento da humidade com um valor numérico.' : '';
+    const error3 = !startValid ? 'Utilize uma data válida.' : '';
+    const error4 = !endValid ? 'Utilize uma data válida.' : '';
+
+    res.status(400).json({
+      error: true,
+      data: [],
+      type: `${error1}${error2}${error3}${error4}`,
+    });
   }
 });
 
